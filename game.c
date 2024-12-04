06dff25cff90c53ab0afc003d0d12175a9da4d27
Mohamed Khaled <mohamed.khaled1872002@gmail.com>
2024-12-04 22:58:26 +0200

    Implement game logic with 4 threads

--- game.c
+++ game.c
@@ -29,10 +29,51 @@ void print_grid() {
 // Function to compute next generation of Game of Life
 void* compute_next_gen(void* arg) {
   
-     /* 
-    write your code here 
-    */
+    int row_start = *(int *) arg;
+    int row_end = row_start + 5;
 
+    int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+    int live_neighbors[5][20];
+
+    for(int i = row_start ; i < row_end ; i++){
+
+        for(int j = 0 ; j < 20 ; j++){
+            
+            live_neighbors[i-row_start][j] = 0;
+            for(int k = 0 ; k < 8 ; k++){
+                if(i + dx[k] > 19 || i + dx[k] < 0 || j + dy[k] > 19 || j + dy[k] < 0) //index out of bound check
+                    continue;
+
+                live_neighbors[i-row_start][j] += grid[i+dx[k]][j+dy[k]];
+            }
+        }
+    }
+
+    pthread_barrier_wait(&barrier);
+
+    for(int i = row_start ; i < row_end ; i++){
+
+        for(int j = 0 ; j < 20 ; j++){
+            
+            if(grid[i][j] == 0 && live_neighbors[i-row_start][j] == 3){ //dead cell has 3 live neighbors
+                grid[i][j] = 1;
+            }
+
+            else if(grid[i][j] == 1 && (live_neighbors[i-row_start][j] < 2 || live_neighbors[i-row_start][j] > 3)){ //live cell has less than 2 or more than 3 live neighbors
+                grid[i][j] = 0;
+            }
+
+        }
+    }
+
+
+    int ret = pthread_barrier_wait(&barrier);
+    printf("ret: %d\n", ret);
+    if(ret == -1){
+        print_grid();
+    }
 }
 
 
@@ -70,9 +111,20 @@ int main() {
     initialize_patterns(grid) ;
     pthread_barrier_init(&barrier, NULL, NUM_THREADS);
 
-    /* 
-    write your code here 
-    */
+    pthread_t threads[NUM_THREADS];
+
+    int thread_row_start[] = {0, 5, 10, 15};
+
+    for(int j = 0 ; j < GENERATIONS ; j++){
+        
+        for(int i = 0 ; i < NUM_THREADS ; i++){
+            pthread_create(&threads[i], NULL, compute_next_gen, &thread_row_start[i]);
+        }
+
+        for(int i = 0 ; i < NUM_THREADS ; i++){
+            pthread_join(threads[i], NULL);
+        }
+    }
 
     pthread_barrier_destroy(&barrier);
     return 0;
